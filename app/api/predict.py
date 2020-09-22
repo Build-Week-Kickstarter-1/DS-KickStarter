b0ace64b2d627da3185159fa963199078afa602a
Tristan-Brown1096 <tristan.brown1096@gmail.com>
2020-09-22 15:49:31 -0500

    Updated the predictor with the real model

--- app/api/predict.py
+++ app/api/predict.py
@@ -1,20 +1,27 @@
 import logging
 import random
+import pickle
 
+import numpy as np
+import sklearn
 from fastapi import APIRouter
 import pandas as pd
 from pydantic import BaseModel, Field, validator, confloat
+from typing import Dict
 
 log = logging.getLogger(__name__)
 router = APIRouter()
+dill = open('app\\api\kickstarter.pkl', 'rb')
+model = pickle.load(dill)
 
 
 class Campaign(BaseModel):
     """Use this data model to parse the request body JSON."""
 
     name: str = Field(..., example='The Songs of Adelaide & Abullah')
+    blurb: str = Field(..., example='Phasellus viverra libero eget placerat')
     category: str = Field(..., example='Publishing')
-    currency: str = Field(..., example='GBP')
+    country: str = Field(..., example='GB')
     deadline: str = Field(..., example='2015-10-09')
     goal: confloat(ge=0) = Field(..., example=1000.00)
     launched: str = Field(..., example='2015-08-11')
@@ -23,8 +30,12 @@ class Campaign(BaseModel):
         """Convert pydantic object to pandas dataframe with 1 row."""
 
         df = pd.DataFrame([dict(self)])
-        df['deadline'] = pd.to_datetime(df['deadline'])
-        df['launched'] = pd.to_datetime(df['launched'])
+        df['deadline'] = pd.to_datetime(df['deadline'], format='%Y/%m/%d')
+        df['launched'] = pd.to_datetime(df['launched'], format='%Y/%m/%d')
+        df = df.drop(['category', 'name', 'blurb', 'country'], axis=1)
+        df['campaign_length'] = df['deadline'] - df['launched']
+        df['campaign_length'] = (df['campaign_length'] / np.timedelta64(1, 'D')).astype(int)
+        df = df.drop(['deadline', 'launched'], axis=1)
         return df
 
 @validator('goal')
@@ -59,33 +70,40 @@ def launched_must_be_string(cls, value):
     assert len(value) == 10, f'length of deadline == {len(value)}, must be == 10'
     assert value
 
+ 
+@validator('country')
+def country_validator(cls, value):
+    """"Validates that the country is a string"""
+    assert type(value) == str, f'country == {value}, must be a string'
+    assert len(value) == 2, f'length of country == {len(value)}, must be == 2'
+    assert value
+
 
 @router.post('/predict')
-async def predict(campaign: Campaign):
+def predict(campaign: Campaign):
     """
-    Make random baseline predictions for classification problem 🔮
 
     ### Request Body
     - `name`: The name of the Kickstarter campaign
+    - `blurb`: The description of the Kickstarter campaign
     - `category`: The main category of the Kickstarter campaign
-    - `currency`: The three letter abbreviation of the currency the Kickstarter is based in
+    - `country`: The ISO 3166-1 alpha 2 code of the country the Kickstarter is based in
     - `deadline`: The end date of the Kickstarter campaign
     - `goal`: The funding goal of the Kickstarter campaign
     - `launched`: The start date of the Kickstarter campaign
 
-    ### Other Categories to be added soon
 
     ### Response
-    - `prediction`: boolean, at random
+    - `prediction`: boolean, 1 for success and 0 for failure
     - `predict_proba`: float between 0.5 and 1.0, 
     representing the predicted class's probability
 
-    Replace the placeholder docstring and fake predictions with your own model.
     """
 
-    X_new = campaign.to_df()
-    log.info(X_new)
-    y_pred = random.choice([True, False])
+
+    X = campaign.to_df()
+    log.info(X)
+    y_pred = model.predict(X)
     return {
-        'prediction': y_pred
+        'prediction': y_pred[0]
     }
